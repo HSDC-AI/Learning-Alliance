bfb1275890faff01ec32ce158f23df34353956aa
xt03337 <xt03337>
2025-06-17 09:54:28 +0800

    新增对应的demo

--- docs/S/LangChain学习记录/demo/getchat.py
+++ docs/S/LangChain学习记录/demo/getchat.py
@@ -4,8 +4,8 @@ def get_chat(model):
     return ChatOpenAI(
         temperature=0,
         model=model,
-        base_url="http://10.255.4.108:8080/v1",  # 根据你的实际API路径确认是否加 /v1
-        api_key="sk-3BEJwQPhsyVSzDW2C963Af69A6Bf4b608810Dd78E2Bb4452"  # 即使是假的，也要传
+        base_url="http://10.255.4.108:8080/v1",
+        api_key="sk-3BEJwQPhsyVSzDW2C963Af69A6Bf4b608810Dd78E2Bb4452"
     )
 
 def get_key(key):
